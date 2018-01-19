2c8b2528e3abe4d786e285584efe5aa0ba6f9803
Daniel França <daniel.franca@protonmail.com>
2018-01-19 22:50:31 +0100

    Fixed static files location

--- backend/app/hue/hue/settings.py
+++ backend/app/hue/hue/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/home/docker/volatile/static'
 
 try:
     from settings_local import *
